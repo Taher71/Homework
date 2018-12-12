148ede31f2121c6e1148c45a42259079414cbcd1
Lucky <lucky@Luckys-MacBook-Pro.local>
2018-12-12 17:56:01 -0500

    first coomit

---
+++ abcd/pnt.java
@@ -0,0 +1,23 @@
+package abcd;
+
+public class pnt {
+
+    public static void main(String[] args) {
+
+        String first_name = "Md";
+        String last_name = "Taher";
+        String student_id = "3154NY";
+        String address = "24 50 94th street NY 11369";
+        System.out.println("My name is " +first_name+" "+last_name);
+        System.out.println(" My student ID id " +student_id);
+        System.out.println(" I live in " +address);
+        System.out.println(" My introduction: " +first_name+ " " +last_name+ " "+student_id+ " " +address);
+
+
+
+
+    }
+
+
+
+}
